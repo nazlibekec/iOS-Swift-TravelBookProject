690efdcc4baeb2bef4eeed71ca2731951f67812c
nazlibekec <nazlibekec@gmail.com>
2023-01-26 12:40:45 +0300

    Pin oluşturuldu.

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -11,8 +11,10 @@ import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
-    @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +28,37 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.startUpdatingLocation()
         
         
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation))
+        
+        
+        //Kullanıcının ne kadar süreyle bastığını belirleme.
+        
+        gestureRecognizer.minimumPressDuration = 3
+        
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
+    @objc func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            
+            //Dokunulan noktayı al.
+            let touchedPoint = gestureRecognizer.location(in: mapView)
+            //Dokunulan noktanın koordinatlarını al.
+            let touchedCoordinate = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            //Pin oluştur.
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinate
+            annotation.title = "New Annotation"
+            annotation.subtitle = "Travel Book"
+            self.mapView.addAnnotation(annotation)
+            
+        }
+        
+    }
+    
     
     //update edilen location ları dizi şeklinde veriyor.
     @objc func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -36,7 +67,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         //Zoom
         
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
